0a006c677626e502152ce14c0c5eb3e05f98a219
Nasr-905 <nasrudeenoladimeji@gmail.com>
2025-02-26 19:34:58 -0500

    Quartz sync: Feb 26, 2025, 7:34 PM

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -55,11 +55,11 @@ export default ((opts: Options) => {
   }
 
   Comments.afterDOMLoaded = script
-  Comments.css = `
-  .giscus {
-    margin: 0 1rem;
-  }
-  `
+  // Comments.css = `
+  // .giscus {
+  //   padding: 0 1rem;
+  // }
+  // `
 
   return Comments
 }) satisfies QuartzComponentConstructor<Options>
