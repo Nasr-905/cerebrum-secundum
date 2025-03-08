d047e63e84ca364a6b80b906cbd4f42b69e0a1d4
Nasr-905 <nasrudeenoladimeji@gmail.com>
2025-03-08 06:50:53 -0500

    Quartz sync: Mar 8, 2025, 6:50 AM

--- quartz/components/Date.tsx
+++ quartz/components/Date.tsx
@@ -15,6 +15,8 @@ export function getDate(cfg: GlobalConfiguration, data: QuartzPluginData): Date 
       `Field 'defaultDateType' was not set in the configuration object of quartz.config.ts. See https://quartz.jzhao.xyz/configuration#general-configuration for more details.`,
     )
   }
+  console.log(data.frontmatter?.title)
+  console.log(data.dates)
   return data.dates?.[cfg.defaultDateType]
 }
 
