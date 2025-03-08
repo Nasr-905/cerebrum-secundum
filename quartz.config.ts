d96af996839d576c1ab379beada772aa097ef9fb
Nasr-905 <nasrudeenoladimeji@gmail.com>
2025-03-08 17:35:28 -0500

    Quartz sync: Mar 8, 2025, 5:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     generateSocialImages: {
       colorScheme: "darkMode",
-      excludeRoot: false,
+      excludeRoot: true,
     },
     theme: {
       fontOrigin: "googleFonts",
