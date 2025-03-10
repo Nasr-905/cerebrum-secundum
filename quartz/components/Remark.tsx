a9cfc7888bea4efd02c9be2e08204af1dd986cb5
Nasr-905 <nasrudeenoladimeji@gmail.com>
2025-03-10 18:08:12 -0400

    Quartz sync: Mar 10, 2025, 6:08 PM

--- quartz/components/Remark.tsx
+++ quartz/components/Remark.tsx
@@ -6,30 +6,15 @@ export default ((userOpts?: Options) => {
 
     function Remark() {
         return (
-          <div className="remark-container">
-            <h2>Join the Discussion</h2>
-            <p>We'd love to hear your thoughts! Leave a comment below:</p>
-            <div id="remark42"></div>
-          </div>
+          <>
+            <hr />
+              <h2>Join the Discussion with <a href="https://remark42.com/" class={"internal"} target={"_blank"}>Remark42</a></h2>
+              <div id="remark42"></div>
+          </>
         );
     }
 
     Remark.css = `
-      .remark-container {
-        margin-top: 2rem;
-        padding: 1rem;
-        border: 1px solid var(--darkgray);
-        border-radius: 8px;
-        background-color: var(--lightgray);
-      }
-      .remark-container h2 {
-        margin-top: 0;
-        font-size: 1.5rem;
-      }
-      .remark-container p {
-        margin-bottom: 1rem;
-        font-size: 1rem;
-      }
       #remark42 {
         margin-top: 1rem;
       }
